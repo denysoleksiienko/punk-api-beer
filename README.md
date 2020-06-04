74c66842457a5dde14cb65487da0bf58aa67e168
Denis Alekseenko <den.effective@gmail.com>
2020-06-04 14:10:33 +0300

    init app/packages

---
+++ README.md
@@ -0,0 +1,7 @@
+## React App Beer
+
+### `npm i`
+
+### `npm start`
+
+#### API: https://punkapi.com/documentation/v2
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './App.scss';
+
+export const App = () => {
+  return <h1>App</h1>;
+};
