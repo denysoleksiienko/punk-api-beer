6a885e9b3f69319718c5d4d2797a65bcbb40be90
Denis Alekseenko <den.effective@gmail.com>
2020-06-05 12:27:03 +0300

    add card component

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
-export const BeerCard = () => {
-  return <div></div>;
+export const BeerCard = ({ beer }) => {
+  const history = useHistory();
+  const { url } = useRouteMatch();
+  return (
+    <section onClick={() => history.push(`${url}/${beer.id}`)} key={beer.id} className="beer-card-content">
+      <div className="beer-card">
+        <h2 className="beer-title">{beer.name}</h2>
+        <img src={beer.image_url} alt={beer.name} />
+      </div>
+    </section>
+  );
 };
